b67028dcb8de68c575985fcf721a11114331279a
Pavel <kosenkov.paul@gmail.com>
2021-02-24 19:30:41 +0700

    files from chapter 11

---
+++ 11/TestExceptions.java
@@ -0,0 +1,26 @@
+//не работает
+public class TestExceptions{
+	public static void main(String[] args){
+		String test = "Нет";
+		try{
+			System.out.println("Начало try");
+			doRisky(test);
+			System.out.println("Конец try");
+		}
+		catch(ScaryException se){
+			System.out.println("Жуткое исключение");
+		}
+		finally{
+			System.out.println("finally");
+		}
+		System.out.println("конец main");
+	}
+	static void doRisky(String test) throws ScaryException{
+		System.out.println("Начало опасного метода");
+		if("yes".equals(test)){
+			throw new ScaryException();
+		}
+		System.out.println("Конец опасного метода");
+		return;
+	}
+}
